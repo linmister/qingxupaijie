cf21ea1ddfde8cd50bd945437196ec24cadd677d
linmister <51257978+linmister@users.noreply.github.com>
2025-10-19 20:50:01 +0800

    配置GitHub Pages自动部署

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/qingxupaijie/', // GitHub Pages部署路径
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
